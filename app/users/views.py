a0cfa4cce17c97f8c3d5c26ec7087ba5a1e00238
Gary Ye <gye@student.tgm.ac.at>
2015-04-27 22:42:30 +0200

    implemented logging out and a render_link function

--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,9 @@
 from app.users.forms import LoginForm
 from flask import Blueprint, flash, redirect, url_for, render_template, request
-from flask.ext.login import login_user
+from flask.ext.login import login_user, login_required, logout_user
 
 mod = Blueprint('users', __name__, template_folder='templates')
+
 
 @mod.route('/login/', methods=('GET', 'POST'))
 def login():
@@ -12,3 +13,11 @@ def login():
         flash("Login successful")
         return redirect(url_for('tracking.index'))
     return render_template('users/login.html', form=form)
+
+
+@mod.route('/logout/')
+@login_required
+def logout():
+    # Tell Flask-Login to destroy the ession->User connection for this session.
+    logout_user()
+    return redirect(url_for('tracking.index'))
